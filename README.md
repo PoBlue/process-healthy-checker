894cd3fc1c438bf1c0cbe72f3a2a20b30ff68dbe
PoBlue <250299430@qq.com>
2017-11-26 22:12:01 +0800

    [README] imporve

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # process-healthy-checker
 check if the process is alive and notify with mail
+
+## 如何使用
+0. 新建并设置 `data.py` 文件
+1. 运行 `python process-checker.py`
+
+```
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+user_account = '' # qq 账号
+stmp_code='' # qq 的 stmp code
+
+# 要检查的 process 的名字, 如：
+process_name = "getReview.py"
+
+# email 的内容，content 为内容，subject 为主题, to_mail 为收件的邮箱地址
+content = "process {name} is dead, fix it right now".format(name=process_name)
+subject = "Process Healthy Checker"
+to_mail = 'xxxx@qq.com'
+```
--- process-checker.py
+++ process-checker.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from data import user_account, password, to_mail, stmp_code
+from data import user_account, to_mail, stmp_code, process_name, content, subject
 from utils import check_process_alive, create_msg, send_qq_mail
 
 def main():
-    process_name = "getReview.py"
-    content = "process {name} is dead, fix it right now".format(name=process_name)
-    subject = "Process Healthy Checker"
-
     if(not check_process_alive(process_name)):
         msg = create_msg(content, subject, user_account)
         send_qq_mail(user_account, stmp_code, to_mail, msg)
