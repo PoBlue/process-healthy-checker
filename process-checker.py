23a8a9cf0004bdd5795d2ded9546ef9fdcf4e056
PoBlue <250299430@qq.com>
2017-12-13 23:10:05 +0800

    [feature] check mutil process

--- process-checker.py
+++ process-checker.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from data import user_account, to_mail, stmp_code, process_name, content, subject
+from data import user_account, to_mail, stmp_code, process_names, content, subject
 from utils import check_process_alive, create_msg, send_qq_mail
 
 def main():
-    if(not check_process_alive(process_name)):
-        msg = create_msg(content, subject, user_account)
-        send_qq_mail(user_account, stmp_code, to_mail, msg)
-        print("is dead")
-    else:
-        print("still alive")
+    for process_name in process_names:
+        if(not check_process_alive(process_name)):
+            msg = create_msg(content.format(name=process_name), subject, user_account)
+            send_qq_mail(user_account, stmp_code, to_mail, msg)
+            print("is dead")
+        else:
+            print("still alive")
     pass
 
 if __name__ == '__main__':
